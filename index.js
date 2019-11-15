1afdf78908beecfc23cbdfd19587bad8ce169bfb
Josi Reis-West <josialexa85@gmail.com>
2019-11-15 14:42:46 -0600

    Completed step 2

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log('Hello World');
+
+let count = 0;
+
+const increase = () => {
+    count++;
+    console.log(count);
+};
+
+const decrease = () => {
+    count--;
+    console.log(count);
+};
+
+const reset = () => {
+    count = 0;
+    console.log(count);
+};
